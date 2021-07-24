f7dc1017628db5fb1a6313c6d41bf1733fae3a4e
enesykaya <enesykaya@gmail.com>
2021-07-24 20:06:58 +0300

    msm8998-common: Build light hal

--- common.mk
+++ common.mk
@@ -200,6 +200,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    lights.msm8998
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
