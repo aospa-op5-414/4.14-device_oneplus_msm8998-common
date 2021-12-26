a4e18d9bc2c9f1d97cfcbc7e74e5c90ada9bc3ca
enesykaya <enesykaya@gmail.com>
2021-12-26 18:07:04 +0300

    msm8998-common: build android.hidl.allocator@1.0
    dlopen failed: library android.hidl.allocator@1.0.so not found: needed by /vendor/lib64/hw/vendor.qti.hardware.qseecom@1.0-impl.so in namespace

--- common.mk
+++ common.mk
@@ -186,6 +186,8 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.allocator@1.0 \
+    android.hidl.allocator@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
