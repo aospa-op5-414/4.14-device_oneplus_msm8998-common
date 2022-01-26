d2eb5427eaa889f86a51d87a0e5bff7b55bf4e3d
enesykaya <enesykaya@gmail.com>
2022-01-26 19:57:55 +0300

    msm8998-common: Update bluetooth from LA.UM.9.12.r1-13300-SMxx50.QSSI12.0

--- common.mk
+++ common.mk
@@ -89,14 +89,11 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    audio.bluetooth.default \
-    android.hardware.bluetooth@1.0 \
-    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth@1.0.vendor \
     com.dsi.ant@1.0.vendor \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
-    libbluetooth_audio_session \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    liba2dpoffload \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
