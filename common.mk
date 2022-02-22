49565321e16cd45ec81dcc8dde13e571c3dc7d6b
enesykaya <enesykaya@gmail.com>
2022-02-22 12:25:29 +0300

    Revert "msm8998-common: drop light hal from tree"
    Roberto ported old driver from 4.4 yay
    
    This reverts commit 6c601ee1b3b7864c94bb6adc2c5516c6a132c2e0.

--- common.mk
+++ common.mk
@@ -188,6 +188,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0.vendor
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.oneplus_msm8998
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
